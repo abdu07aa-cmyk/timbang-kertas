6d16c7e17b8fea3ba980bb4e61c12fdd65f6146b
abdu07aa-cmyk <abdu07.aa@gmail.com>
2026-04-06 14:20:22 +0700

    Add initial CSS styles for body and table

---
+++ style.js
@@ -0,0 +1,20 @@
+body{
+font-family: Arial;
+padding:20px;
+}
+
+table{
+border-collapse: collapse;
+width:100%;
+margin-bottom:20px;
+}
+
+th,td{
+border:1px solid #ccc;
+padding:8px;
+text-align:left;
+}
+
+th{
+background:#f2f2f2;
+}
